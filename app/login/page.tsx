1313914913d315c72160c96e26ee4986b2a4925f
Alberto Sierra <Sierra.Alberto@iCloud.com>
2024-07-25 21:22:24 -0700

    small update to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,13 +2,34 @@ import { login, signup } from "./actions";
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className="flex mx-auto justify-center">
+      <form className="p-10 bg-slate-500 rounded-2xl text-black">
+        <label className="pr-5" htmlFor="email">
+          Email:
+        </label>
+        <input
+          className="rounded-xl"
+          id="email"
+          name="email"
+          type="email"
+          required
+        />
+        <label className="pl-5 pr-5" htmlFor="password">
+          Password:
+        </label>
+        <input
+          className="rounded-xl"
+          id="password"
+          name="password"
+          type="password"
+          required
+        />
+        <br />
+        <div className="flex justify-between pt-5">
+          <button formAction={login}>Log in</button>
+          <button formAction={signup}>Sign up</button>
+        </div>
+      </form>
+    </div>
   );
 }
